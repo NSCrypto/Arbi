9b7462481defca9d96d9d8a919d44ec00d6e81fc
Travis Savo <evilgenius@nefariousplan.com>
2015-04-11 02:43:59 -0700

    Fix for exchanges

--- Arbi/src/test/java/com/tsavo/arbi/TickerDatabaseTest.java
+++ Arbi/src/test/java/com/tsavo/arbi/TickerDatabaseTest.java
@@ -9,16 +9,26 @@ import org.junit.Test;
 
 import com.tsavo.trade.db.LiveTicker;
 import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeFactory;
+import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.bitfinex.v1.BitfinexExchange;
+import com.xeiam.xchange.btce.v3.BTCEExchange;
+import com.xeiam.xchange.bter.BTERExchange;
+import com.xeiam.xchange.cryptsy.CryptsyExchange;
+import com.xeiam.xchange.currency.CurrencyPair;
 import com.xeiam.xchange.exceptions.ExchangeException;
 import com.xeiam.xchange.exceptions.NotAvailableFromExchangeException;
 import com.xeiam.xchange.exceptions.NotYetImplementedForExchangeException;
+import com.xeiam.xchange.kraken.KrakenExchange;
+import com.xeiam.xchange.okcoin.OkCoinExchange;
+import com.xeiam.xchange.yacuna.YacunaExchange;
 
 public class TickerDatabaseTest {
 
 	@Test
 	public void test() throws ExchangeException, NotAvailableFromExchangeException, NotYetImplementedForExchangeException, IOException, InterruptedException {
 		List<Exchange> exchanges = new ArrayList<>();
-
+		
 		List<LiveTicker> tickers = new ArrayList<>();
 		exchanges.forEach(exchange -> tickers.add(new LiveTicker(exchange)));
 		while (true) {
@@ -26,11 +36,10 @@ public class TickerDatabaseTest {
 			when.setHours(0);
 			when.setMinutes(0);
 			when.setSeconds(0);
-			// tickers.forEach(ticker -> {
-			// System.out.println(ticker.db.exchangeName + " "
-			// + ticker.getDataForTimeframe(new CurrencyPair("BTC", "USD"),
-			// when, new Date(new Date().getTime() + 1000000), 1000 * 60 * 5));
-			// });
+			tickers.forEach(ticker -> {
+				System.out.println(ticker.db.exchangeName + " "
+						+ ticker.getDataForTimeframe(new CurrencyPair("BTC", "USD"), when, new Date(new Date().getTime() + 1000000), 1000 * 60 * 5));
+			});
 
 			Thread.sleep(1000);
 		}
